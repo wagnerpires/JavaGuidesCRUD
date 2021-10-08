f050c8ce4a75938656eecfd677f2a6fd906b3a72
Wagner Pires <wagnerpires@hotmail.com>
2021-10-07 21:34:30 -0300

    Update employee endpoint

--- backend/src/main/java/com/wrtecnologia/javaguidescrud/controller/EmployeeController.java
+++ backend/src/main/java/com/wrtecnologia/javaguidescrud/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.wrtecnologia.javaguidescrud.controller;
 
+import com.wrtecnologia.javaguidescrud.exception.ResourceNotFoundException;
 import com.wrtecnologia.javaguidescrud.model.Employee;
 import com.wrtecnologia.javaguidescrud.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +27,28 @@ public class EmployeeController {
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    // build get employee by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    // build update employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+
+        return ResponseEntity.ok(updateEmployee);
     }
 }
